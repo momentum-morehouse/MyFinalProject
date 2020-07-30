36e6da70db37a588d849aff681dc90003ccb6871
Michael  Abraha <m.b.abraha@msmary.edu>
2020-07-30 14:28:22 +0000

    update test

--- finalproject/urls.py
+++ finalproject/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf import settings
+#from django.conf import settings
 from django.urls import path
-from myapp import views as myapp_views
+#from myapp import views as myapp_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
